6487daf1e930be369b38ca184bde54f82f647ce7
Andreas Lorer <contact@andreaslorer.de>
2015-01-01 13:31:56 +0100

    added build task

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,42 @@ var notify = require('gulp-notify');
 var concat = require('gulp-concat');
 var glob = require('glob');
 var del = require('del');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 
 
 // minify javascript
 gulp.task('js', function () {
-    gulp.src('js/*.js')
+    gulp.src('app/js/*.js')
     	.pipe(concat('multiColumn.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js/'))
         .pipe(notify({ message: 'Finished minifying JavaScript'}));
 });
 
+// copy all files from /app/
+gulp.task('copy', function () {
+  return gulp.src([
+  	'app/**/*',
+  	'!app/assets/**/*.jpg',
+  	'!app/*.html',
+  	'!app/js/*.js'
+  	],{
+  	 dot:true
+  	}).pipe(gulp.dest('dist'))
+      .pipe($.size({title: 'copy'}));
+});
+
 // watch task
 gulp.task('watch', function () {
-    gulp.watch('js/**/*.js', ['js']);
+    gulp.watch('app/js/**/*.js', ['js']);
 });
 
 // Clean Output Directory
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 gulp.task('default', ['js', 'watch']);
+
+gulp.task('build', ['clean'], function (cb) {
+  runSequence('js', ['copy'], cb);
+});
